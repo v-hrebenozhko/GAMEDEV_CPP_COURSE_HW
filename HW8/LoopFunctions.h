beab42df7f6b82e1cf50abff30bcb92d93209207
kigdav3rd <kigdav3rd@gmail.com>
2024-05-16 16:20:32 +0300

    Added HomeWork8

---
+++ HW8/LoopFunctions.h
@@ -0,0 +1,14 @@
+#pragma once
+
+// Task 1
+
+void SignCounter(int NumbersAmount);
+
+// Task 2
+
+int LoopFactorial(int Count);
+
+//Task 3
+
+void PrintNaturalNumbersHigh(int Count);
+void PrintNaturalNumbersLow(int Count);
